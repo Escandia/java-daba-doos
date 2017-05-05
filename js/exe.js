b6396801d06fee84cc90f3e2697da5f561ad5b39
Escandia Camargo <Escandia@users.noreply.github.com>
2017-05-05 10:06:23 +0200

    Web Page java-daba-doos
    Java-daba-doos Team

---
+++ js/exe.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+               document.formclave.onsubmit = acceso;
+           }
+           function acceso(){
+               alert(document.formclave.clave.value);
+           }
+
+
+var min = 1;
+var max = 100;
+var chances = 0;
+var random = Math.floor(Math.random() * max - min + 1) + min;
+
+function gameNumber(){
+
+ var numberInput=document.getElementById("number").value;
+ chances = chances + 1;
+
+ if (numberInput >= min && numberInput <= max ) {
+     if (numberInput < random) {
+       textError.innerHTML = "Introduce un número más alto";
+     } else if (numberInput > random){
+       textError.innerHTML = "Introduce un número más bajo";
+     } else {
+       textError.innerHTML = "Correcto has acertado!! Con " + chances + " intentos";
+       document.getElementById('audioJava').play();
+     }
+ } else {
+   textError.innerHTML = "Introduce un número valido, Neandertal!!.";
+ }
+}
+
+// function validar(e){
+//   var tecla = (document.all) ? e.keyCode : e.which;
+//   if (tecla == 13){
+//   gameNumber();
+//   }
+// }
