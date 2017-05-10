1d7e50fe6f150c79b7beea2cd752b10285178374
puyhg5 <puyab500@gmail.com>
2017-05-10 12:39:45 +0200

    WEB WITH MINIMUM REQUIREMENTS
    Web completed with minimum requirements

---
+++ scripts/game1.js
@@ -0,0 +1,44 @@
+var min = 1;
+var max = 100;
+var chances = 0;
+var random = Math.floor(Math.random() * max - min + 1) + min;
+
+function gameNumber(){
+
+ var numberInput=document.querySelector("#number").value;
+ chances = chances + 1;
+
+ if (numberInput >= min && numberInput <= max ) {
+     if (numberInput < random) {
+       textError.innerHTML = "Sube al glaciar";
+     } else if (numberInput > random){
+       textError.innerHTML = "Baja a la caverna";
+     } else {
+       textError.innerHTML = "Correcto has acertado!! Con " + chances + " intentos";
+       document.getElementById('audioJava').play();
+     }
+ } else {
+   textError.innerHTML = "Introduce un número valido, ¡Neandertal!";
+ }
+}
+
+var inputEnter = document.querySelector("#number");
+
+function onKeyPress(event){
+  if(event.charCode === 13){
+    gameNumber();
+  }
+}
+
+function fade(button) {
+  if (button.value === "FadeOut") {
+    document.querySelector("#textError").className = "fade-out";
+    button.value = "FadeIn";
+  }
+  else{
+    document.querySelector("#textError").className = "fade-in";
+    button.value = "FadeOut";
+  }
+}
+
+inputEnter.addEventListener("keypress", onKeyPress);
---
+++ scripts/game2.js
@@ -0,0 +1,115 @@
+//Declarar y asignar las posibles caras de las cartas
+var cards = [{photo1: "b1", photo2:"b2"},
+{photo1: "d1", photo2:"d2"},
+{photo1: "e1", photo2:"e2"},
+{photo1: "l1", photo2:"l2"},
+{photo1: "p1", photo2:"p2"}];
+
+//Generar 3 números aleatorios para coger 3 parejas de las 5 posibles y almacenando en un array
+var min = 0;
+var max = 5;
+var randomCoupleOne = Math.floor(Math.random() * max - min) + min;
+var randomCoupleTwo = Math.floor(Math.random() * max - min) + min;
+while (randomCoupleOne === randomCoupleTwo){
+  randomCoupleTwo = Math.floor(Math.random() * max - min) + min;
+}
+var randomCoupleThree = Math.floor(Math.random() * max - min) + min;
+while (randomCoupleOne === randomCoupleThree || randomCoupleTwo === randomCoupleThree){
+  randomCoupleThree = Math.floor(Math.random() * max - min) + min;
+}
+var position = [randomCoupleOne, randomCoupleTwo, randomCoupleThree];
+
+//Separar en un array todas las caras
+var choosenCards = [];
+for (var i=0; i<position.length; i++){
+  var actualCardOne = cards[position[i]].photo1;
+  var actualCardTwo = cards[position[i]].photo2;
+  choosenCards.push(actualCardOne, actualCardTwo);
+}
+
+//Barajar las caras
+function shuffle(choosenCards){
+  firstIndexCards = 0;
+  lastIndexCards = choosenCards.length - 1;
+  for (var iteration=1; iteration<=10; iteration++){
+    var indexRandom = Math.floor(Math.random() * (lastIndexCards - firstIndexCards) + firstIndexCards);
+    var indexRandom2 = Math.floor(Math.random() * (lastIndexCards - firstIndexCards) + firstIndexCards);
+    while (indexRandom === indexRandom2){
+      var indexRandom2 = Math.floor(Math.random() * (lastIndexCards - firstIndexCards) + firstIndexCards);
+    }
+    var change = choosenCards[indexRandom];
+    var change2 = choosenCards[indexRandom2];
+    choosenCards[indexRandom] = change2;
+    choosenCards[indexRandom2] = change;
+  }
+}
+shuffle(choosenCards);
+
+
+//Asignar a cada div su cara
+var faceCardOne = document.querySelectorAll(".front");
+for (var j=0; j<faceCardOne.length; j++){
+  var urlCard = "../img/cards/"+choosenCards[j]+".png";
+  faceCardOne[j].setAttribute("src", urlCard);
+}
+
+//Rotar las cartas al seleccionarlas
+var upCards = [];
+var returnCards = [];
+var tirada = 0;
+function rotate(event){
+  console.log(this);
+  this.style.transform = "rotateY(180deg)";
+  returnCards.push(this);
+  var sourceCurrentCardFace = this.children[1].children[0].getAttribute("src");
+  upCards.push(sourceCurrentCardFace[13]);
+  tirada = tirada + 1;
+  if (tirada === 2) {
+    compare(upCards);
+  }
+  console.log(upCards);
+  console.log(returnCards);
+}
+
+var rotateCards = document.querySelectorAll(".rotate-card");
+
+for (var i = 0; i < rotateCards.length; i++) {
+  rotateCards[i].addEventListener("click", rotate);
+}
+
+//Comparar las caras seleccionadas y actuar en consecuencia
+var score = 0;
+var fail = -5;
+var win = 10;
+var correctCouple = 0;
+
+function compare(variable){
+  var totalScore = document.querySelector(".total-score-text");
+  var oneScore = document.querySelector(".score-text");
+  if (variable[0] === variable[1]) {
+    score = score + win;
+    correctCouple++;
+    console.log(correctCouple);
+    totalScore.innerHTML = score;
+    oneScore.innerHTML = win;
+    if (correctCouple === 3) {
+      var modal = document.querySelector(".modal");
+      modal.style.display = "flex";
+    }
+    console.log(score);
+  }else{
+    console.log(returnCards[0]);
+    returnCards[0].style.transform="rotateY(360deg)";
+    returnCards[1].style.transform="rotateY(360deg)";
+    if (score>=5) {
+      score = score + fail;
+    } else {
+      score = 0;
+    }
+    totalScore.innerHTML = score;
+    oneScore.innerHTML = fail;
+  }
+  variable.splice(0,2);
+  tirada=0;
+  returnCards.splice(0,2);
+}
