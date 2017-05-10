eb23053ed7671e0720d3c84a18be4fdc6d3cfa15
puyhg5 <puyab500@gmail.com>
2017-05-11 00:20:23 +0200

    GAME 3 CORRECTED

--- scripts/game3.js
+++ scripts/game3.js
@@ -8,6 +8,9 @@ function validate(){
   var favSmoUser = document.querySelector("#favourite-smoothie").value.toLowerCase();
 
   if (favVarUser === favVar && numTerminalUser === numTerminal && favSmoUser === favSmo){
+    document.querySelector("#favourite-variable").style.borderColor = "green";
+    document.querySelector("#number-terminal").style.borderColor = "green";
+    document.querySelector("#favourite-smoothie").style.borderColor = "green";
     var modal = document.querySelector(".modal");
     var textModal = document.querySelector(".score-game3");
     document.getElementById('audioJava').play();
@@ -18,12 +21,14 @@ function validate(){
       document.querySelector("#favourite-variable").style.borderColor = "red";
       if (numTerminalUser !== numTerminal) {
         document.querySelector("#number-terminal").style.borderColor = "red";
-        if (favSmoUser === favSmo) {
+        if (favSmoUser !== favSmo) {
           document.querySelector("#favourite-smoothie").style.borderColor = "red";
+        }else{
+          document.querySelector("#favourite-smoothie").style.borderColor = "green";
         }
       }else{
         document.querySelector("#number-terminal").style.borderColor = "green";
-        if (favSmoUser === favSmo) {
+        if (favSmoUser !== favSmo) {
           document.querySelector("#favourite-smoothie").style.borderColor = "red";
         }else{
           document.querySelector("#favourite-smoothie").style.borderColor = "green";
@@ -31,16 +36,16 @@ function validate(){
       }
     }else{
       document.querySelector("#favourite-variable").style.borderColor = "green";
-      if(numTerminalUser !== numTerminal){
+      if (numTerminalUser !== numTerminal) {
         document.querySelector("#number-terminal").style.borderColor = "red";
-        if (favSmoUser === favSmo) {
+        if (favSmoUser !== favSmo) {
           document.querySelector("#favourite-smoothie").style.borderColor = "red";
         }else{
           document.querySelector("#favourite-smoothie").style.borderColor = "green";
         }
       }else{
         document.querySelector("#number-terminal").style.borderColor = "green";
-        if (favSmoUser === favSmo) {
+        if (favSmoUser !== favSmo) {
           document.querySelector("#favourite-smoothie").style.borderColor = "red";
         }else{
           document.querySelector("#favourite-smoothie").style.borderColor = "green";
@@ -50,19 +55,19 @@ function validate(){
   }
 }
 
-// var inputEnter1 = document.querySelector("#favourite-variable");
-// var inputEnter2 = document.querySelector("#number-terminal");
-// var inputEnter3 = document.querySelector("favourite-smoothie");
-//
-// function onKeyPress(event){
-//   if(event.charCode === 13){
-//     validate();
-//   }
-// }
-//
-// inputEnter1.addEventListener("keypress", onKeyPress);
-// inputEnter2.addEventListener("keypress", onKeyPress);
-// inputEnter3.addEventListener("keypress", onKeyPress);
+var inputEnter1 = document.querySelector("#favourite-variable");
+var inputEnter2 = document.querySelector("#number-terminal");
+var inputEnter3 = document.querySelector("#favourite-smoothie");
+
+function onKeyPress(event){
+  if(event.charCode === 13){
+    validate();
+  }
+}
+
+inputEnter1.addEventListener("keypress", onKeyPress);
+inputEnter2.addEventListener("keypress", onKeyPress);
+inputEnter3.addEventListener("keypress", onKeyPress);
 
 // JS button popUp
 var restart = document.querySelector("#domInst");
